8840b6edc62e2f1d10a3497f457f4c65825ecd70
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-03-02 16:28:42 +0200

    req

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,15 @@ function App() {
       <Modal active={modalActive} setActive={setModalActive}>
         <h2 className="h2">Modal Window</h2>
         <p className="p">This is a modal content.</p>
+        <from className="form">
+          <label htmlFor="">1</label>
+          <input type="text" />
+          <label htmlFor="">2</label>
+          <input type="text" />
+          <label htmlFor="">3</label>
+          <input type="text" />
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </from>
         <button onClick={() => setModalActive(false)}>Close</button>
       </Modal>
     </>
