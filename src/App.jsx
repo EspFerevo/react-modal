6b68fe69a742b8bb01d8a3c8f3f45df94b4811bb
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-03-02 16:22:09 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import Modal from "./modal/modal.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [modalActive, setModalActive] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div>
+          <button onClick={() => setModalActive(true)}>
+            Open Modal Window
+          </button>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat,
+            tenetur iste deleniti id quisquam, aliquid dignissimos aliquam
+            tempore, eveniet repudiandae veritatis dolor. Assumenda numquam vero
+            cum ipsa! Cum, quae suscipit.
+          </p>
+        </div>
+      </main>
+
+      <Modal active={modalActive} setActive={setModalActive}>
+        <h2>Modal Window</h2>
+        <p>This is a modal content.</p>
+        <button onClick={() => setModalActive(false)}>Close</button>
+      </Modal>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
